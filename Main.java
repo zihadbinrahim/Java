b535aae2cc12431d6e6f6ab1fb15c4b8c58c08c4
Zihad Bin Rahim <145361457+zihadbinrahim@users.noreply.github.com>
2024-01-21 22:08:16 +0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.print("ID:23-52993-3");
         System.out.print("Depertment: Computer Science");
         System.out.print("Semester: 2nd");
+        System.out.print("Versity: AIUB");
     }
 }
