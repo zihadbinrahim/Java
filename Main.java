3b797647243e6da46e5979a134d77de7c900f743
zihadbinrahim <145361457+zihadbinrahim@users.noreply.github.com>
2024-01-21 21:56:11 +0600

    Semester: 2nd

--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.print("Zihad");
-        System.out.print("AIUB")
+        System.out.print("Name:Zihad");
+        System.out.print("ID:23-52993-3");
+        System.out.print("Depertment: Computer Science");
+        System.out.print("Semester: 2nd");
     }
 }
