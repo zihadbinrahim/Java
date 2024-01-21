f3ec008c8861c5ec17c01dd6d82aa07ff0b8999b
Zihad Bin Rahim <145361457+zihadbinrahim@users.noreply.github.com>
2024-01-21 21:41:05 +0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.print("Zihad");
-        System.out.print("AIUB");
+        System.out.print("Name:Zihad");
+        System.out.print("ID:23-52993-23");
+        System.out.print("Depertment: Computer Science");
+        
     }
 }
