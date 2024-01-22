8085ad0104e7239516dc0cb946093e7255bcda5e
zihadbinrahim <145361457+zihadbinrahim@users.noreply.github.com>
2024-01-22 20:13:31 +0600

    Create Zihad.java

---
+++ Zihad.java
@@ -0,0 +1,9 @@
+public class Zihad {
+    public static void main(String[] args) {
+        System.out.println("Name:Zihad");
+        System.out.println("ID:23-52993-3");
+        System.out.println("Depertment: Computer Science");
+        System.out.println("Versity Name: AIUB");
+        System.out.println("Year of admition:2023");
+    }
+}
