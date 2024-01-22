67057d6122738c773dab7db70801951d01591e27
zihadbinrahim <145361457+zihadbinrahim@users.noreply.github.com>
2024-01-22 21:37:04 +0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.print("Name:Zihad");
-        System.out.print("ID:23-52993-3");
-        System.out.print("Depertment: Computer Science");
-        System.out.print("Versity Name: AIUB");
-        System.out.print("Year of admition:2023");
+        System.out.println("Name:Zihad");
+        System.out.println("ID:23-52993-3");
+        System.out.println("Depertment: Computer Science");
+        System.out.println("Versity Name: AIUB");
+        System.out.println("Year of admition:2023");
     }
 }
