ceab502b2b6603d7e29297953087bb0f5f78e1c2
zihadbinrahim <145361457+zihadbinrahim@users.noreply.github.com>
2024-02-02 00:10:16 +0600

    Temperature

---
+++ Temperature.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Temperature {
+
+    public static void main(String[] args) {
+		
+		float f,c;
+		
+	Scanner input = new Scanner(System.in);
+		
+	
+	  System.out.print("Enter Farenheit Number:");
+	  f = input.nextFloat();
+	  	  
+	  c = ((5f/9f)*(f-32));
+	  
+	  System.out.println("Coversion to Celsiuds:"+c+" C");
+	  
+	  
+	
+		
+		
+		
+		
+		
+		
+		
+		
+        
+    }
+}
