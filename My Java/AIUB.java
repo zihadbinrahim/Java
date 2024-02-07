f4d724e3356fc52e3272fc05926e23b71d9ed7ab
zihadbinrahim <145361457+zihadbinrahim@users.noreply.github.com>
2024-02-08 01:51:25 +0600

    mainus

--- My Java/AIUB.java
+++ My Java/AIUB.java
@@ -6,17 +6,17 @@ public class AIUB {
      String department;
      
 	 
-	 Scanner input1 = new Scanner (System.in);
+	 Scanner input = new Scanner (System.in);
 	 System.out.print("Enter Your Name: ");
-	 name = input1.nextLine();
+	 name = input.nextLine();
 	 
-	 Scanner input2 = new Scanner (System.in);
+	 Scanner input = new Scanner (System.in);
 	 System.out.print("Enter ID:");
-	 id = input2.nextInt();
+	 id = input.nextInt();
 	 
-	 Scanner input3 = new Scanner (System.in);
+	 Scanner input = new Scanner (System.in);
 	 System.out.print("Enter Your Department Name:");
-	 department = input3.nextLine();
+	 department = input.nextLine();
 	 
 	 System.out.println("Name :"+name);
 	 System.out.println("Your id :"+id);
