e589fd7264d3ad385dd58651730ae59ac810627f
zihadbinrahim <145361457+zihadbinrahim@users.noreply.github.com>
2024-02-15 19:37:58 +0600

    add

---
+++ My Java/Sum.java
@@ -0,0 +1,35 @@
+ import java.util.Scanner;
+ public class Sum{
+	 private int a;
+	 private int b;
+	 
+	 public Sum (int a, int b){ //constructor 
+		 
+		 this.a = a;
+		 this.b = b;
+	 
+	 } 
+	 
+	 public int add(){
+		 return a + b;
+	 }
+	 
+	 public void display (){
+		 System.out.println (add());
+	 }
+	 
+	 public static void main (String[]args){
+		 Scanner input = new Scanner (System.in);
+		 
+		 System.out.print("Enter First Number : ");
+		 int a = input.nextInt();
+		 System.out.print("Enter Second Number : ");
+		 int b = input.nextInt();
+		 
+		 Sum ss = new Sum (a,b); //object
+		 
+		 ss.display();
+		 
+	 }
+	 
+	 }
