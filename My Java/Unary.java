7ec16cf5775918a768dec4bbdcd157d0125a6052
zihadbinrahim <145361457+zihadbinrahim@users.noreply.github.com>
2024-02-16 01:26:38 +0600

    add

---
+++ My Java/Unary.java
@@ -0,0 +1,13 @@
+public class Unary{
+public static void main (String[]args){
+	
+	int x = -10;
+	int result;
+	
+	result =+x; // -10
+	System.out.println("Result = "+result);
+	
+	result =-x; //10
+	System.out.println("Result = "+result);
+}
+}
