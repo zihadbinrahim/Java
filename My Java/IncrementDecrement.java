9a7d937915bccf80ff7786c97b7bba95b5ba975b
zihadbinrahim <145361457+zihadbinrahim@users.noreply.github.com>
2024-02-19 00:32:50 +0600

    add

--- My Java/IncrementDecrement.java
+++ My Java/IncrementDecrement.java
@@ -1,1 +1,13 @@
- 
+public class IncrementDecrement {
+	public static void main (String[] args){
+		
+		int x = 25;
+		int y;
+		
+		y= x++;
+		System.out.println("Y: "+y);
+		y= x;
+		System.out.println("Y: "+y);
+		
+	}
+}
