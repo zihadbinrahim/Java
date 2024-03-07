0263cd16528789e9373aae6b3e16c78c3969982f
zihadbinrahim <145361457+zihadbinrahim@users.noreply.github.com>
2024-03-07 20:17:23 +0600

    add

---
+++ My Java/SubStrings.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class SubStrings {
+    public static void main (String args []){
+        String sentence = "My name is Zihad";
+        String name = sentence.substring(11,sentence.length());
+        System.out.println(name);
+    }
+}
